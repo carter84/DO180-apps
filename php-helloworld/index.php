ec0003ac8b494ae85c5a93e0a23491bc32e7fd87
Student User <student@workstation.lab.example.com>
2020-09-15 12:06:35 -0400

    Update to index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "And on I read, until the day was done. And I sat in regret of all the things I've done.\n";
+print "Hello, World! php version is " . PHP_VERSION . ".<br>";
+print "And on I read, until the day was gone. And I sat in regret of all the things I've done.<br>";
 ?>
